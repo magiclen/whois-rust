a61b504e7a57f3e48ee9c6cf4a0b2554b6fa2554
Magic Len <len@magiclen.org>
2018-11-14 19:16:34 +0800

    tidy the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,49 @@
-//! This is a WHOIS client library for Rust, inspired by https://github.com/hjr265/node-whois
-//!
-//! ## Usage
-//!
-//! You can make a **servers.json** file or copy one from https://github.com/hjr265/node-whois
-//!
-//! This is a simple example of **servers.json**.
-//!
-//! ```json,ignore
-//! {
-//!     "org": "whois.pir.org",
-//!     "": "whois.ripe.net",
-//!     "_": {
-//!         "ip": {
-//!             "host": "whois.arin.net",
-//!             "query": "n + $addr\r\n"
-//!         }
-//!     }
-//! }
-//! ```
-//!
-//! Then, use the `from_path` (or `from_string` if your JSON data is in-memory) associated function to create a `WhoIs` instance.
-//!
-//! ```ignore
-//! extern crate whois_rust;
-//!
-//! use whois_rust::WhoIs;
-//!
-//! let whois = WhoIs::from_path("/path/to/servers.json").unwrap();
-//! ```
-//!
-//! Use the `lookup` method and input a `WhoIsLookupOptions` instance to lookup a domain or an IP.
-//!
-//! ```ignore
-//! extern crate whois_rust;
-//!
-//! use whois_rust::{WhoIs, WhoIsLookupOptions};
-//!
-//! let whois = WhoIs::from_path("/path/to/servers.json").unwrap();
-//!
-//! let result: String = whois.lookup(WhoIsLookupOptions::from_string("magiclen.org").unwrap()).unwrap();
-//! ```
+/*!
+# WHOIS Rust
+
+This is a WHOIS client library for Rust, inspired by https://github.com/hjr265/node-whois
+
+## Usage
+
+You can make a **servers.json** file or copy one from https://github.com/hjr265/node-whois
+
+This is a simple example of **servers.json**.
+
+```json
+{
+    "org": "whois.pir.org",
+    "": "whois.ripe.net",
+    "_": {
+        "ip": {
+            "host": "whois.arin.net",
+            "query": "n + $addr\r\n"
+        }
+    }
+}
+```
+
+Then, use the `from_path` (or `from_string` if your JSON data is in-memory) associated function to create a `WhoIs` instance.
+
+```rust,ignore
+extern crate whois_rust;
+
+use whois_rust::WhoIs;
+
+let whois = WhoIs::from_path("/path/to/servers.json").unwrap();
+```
+
+Use the `lookup` method and input a `WhoIsLookupOptions` instance to lookup a domain or an IP.
+
+```rust,ignore
+extern crate whois_rust;
+
+use whois_rust::{WhoIs, WhoIsLookupOptions};
+
+let whois = WhoIs::from_path("/path/to/servers.json").unwrap();
+
+let result: String = whois.lookup(WhoIsLookupOptions::from_string("magiclen.org").unwrap()).unwrap();
+```
+*/
 
 pub extern crate serde_json;
 pub extern crate validators;
