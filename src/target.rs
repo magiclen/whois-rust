a3e0b0e8c06fe9999e63780901fc69a0b3ea5946
Magic Len <len@magiclen.org>
2020-07-23 20:40:54 +0800

    add a new function `from_host_unchecked` for Target

--- src/target.rs
+++ src/target.rs
@@ -4,3 +4,10 @@ use crate::validators::prelude::*;
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Validator)]
 #[validator(host(port(NotAllow)))]
 pub struct Target(pub(crate) Host);
+
+impl Target {
+    #[inline]
+    pub const unsafe fn from_host_unchecked(host: Host) -> Target {
+        Target(host)
+    }
+}
